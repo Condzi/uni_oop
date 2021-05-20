a188c16bad26f8e2d0b501d57b8ee5e44c0f397f
condzi <Condzi@users.noreply.github.com>
2021-05-20 06:39:33 +0200

    add final project, project

---
+++ final_project/school_journal/source/main.cpp
@@ -0,0 +1,5 @@
+#include "pch.hpp"
+
+int main() {
+  std::cout << "Hello, world!\n";
+}
---
+++ final_project/school_journal/source/pch.hpp
@@ -0,0 +1,24 @@
+#pragma once
+
+#include <algorithm>
+#include <cmath>
+#include <cstdint>
+#include <fstream>
+#include <iostream>
+#include <memory>
+
+namespace sj
+{
+using u8  = std::uint8_t;
+using u16 = std::uint16_t;
+using u32 = std::uint16_t;
+using u64 = std::uint16_t;
+
+using s8  = std::int8_t;
+using s16 = std::int16_t;
+using s32 = std::int16_t;
+using s64 = std::int16_t;
+
+using f32 = float;
+using f64 = double;
+}
